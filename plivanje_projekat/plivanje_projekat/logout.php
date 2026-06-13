f9a27062b63af076c4cf74811023a393848c52de
m-petr01 <mateja00001@gmail.com>
2026-06-14 01:20:46 +0200

    Delete plivanje_projekat/plivanje_projekat directory

--- plivanje_projekat/plivanje_projekat/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/classes/Session.php';
-
-Session::logout();
-
-header('Location: login.php');
-exit;
--- plivanje_projekat/plivanje_projekat/test_database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/config/Database.php';
-
-$database = new Database();
-$connection = $database->connect();
-
-echo 'Uspešno povezivanje sa bazom!';
