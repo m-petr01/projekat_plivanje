64e81d6d1d9b0af975755c36428426fe6a82c76b
m-petr01 <mateja00001@gmail.com>
2026-06-14 12:40:54 +0200

    Add create method to Nagrada class

--- plivanje_projekat/classes/Nagrada.php
+++ plivanje_projekat/classes/Nagrada.php
@@ -4,6 +4,23 @@ require_once __DIR__ . '/BaseModel.php';
 
 class Nagrada extends BaseModel
 {
+    public function create(array $data): bool
+    {
+        $sql = "INSERT INTO nagrade
+                (polaznik_id, naziv, opis, datum)
+                VALUES
+                (:polaznik_id, :naziv, :opis, :datum)";
+
+        $stmt = $this->connection->prepare($sql);
+
+        return $stmt->execute([
+            ':polaznik_id' => $data['polaznik_id'],
+            ':naziv' => $data['naziv'],
+            ':opis' => $data['opis'],
+            ':datum' => $data['datum']
+        ]);
+    }
+
     public function readByPolaznik(int $polaznikId): array
     {
         $sql = "SELECT *
