319d087d065c1e13ab817bc3adcf8a3a7f6dd50f
m-petr01 <mateja00001@gmail.com>
2026-06-14 12:55:08 +0200

    Add CRUD methods to Nagrada class

--- plivanje_projekat/classes/Nagrada.php
+++ plivanje_projekat/classes/Nagrada.php
@@ -7,9 +7,19 @@ class Nagrada extends BaseModel
     public function create(array $data): bool
     {
         $sql = "INSERT INTO nagrade
-                (polaznik_id, naziv, opis, datum)
+                (
+                    polaznik_id,
+                    naziv,
+                    opis,
+                    datum
+                )
                 VALUES
-                (:polaznik_id, :naziv, :opis, :datum)";
+                (
+                    :polaznik_id,
+                    :naziv,
+                    :opis,
+                    :datum
+                )";
 
         $stmt = $this->connection->prepare($sql);
 
@@ -35,5 +45,69 @@ class Nagrada extends BaseModel
         ]);
 
         return $stmt->fetchAll();
+    }
+
+    public function findFirstByPolaznik(
+        int $polaznikId
+    ): array|false {
+        $sql = "SELECT *
+                FROM nagrade
+                WHERE polaznik_id = :polaznik_id
+                ORDER BY datum DESC, id DESC
+                LIMIT 1";
+
+        $stmt = $this->connection->prepare($sql);
+
+        $stmt->execute([
+            ':polaznik_id' => $polaznikId
+        ]);
+
+        return $stmt->fetch();
+    }
+
+    public function update(
+        int $id,
+        array $data
+    ): bool {
+        $sql = "UPDATE nagrade
+                SET
+                    naziv = :naziv,
+                    opis = :opis,
+                    datum = :datum
+                WHERE id = :id";
+
+        $stmt = $this->connection->prepare($sql);
+
+        return $stmt->execute([
+            ':id' => $id,
+            ':naziv' => $data['naziv'],
+            ':opis' => $data['opis'],
+            ':datum' => $data['datum']
+        ]);
+    }
+
+    public function delete(int $id): bool
+    {
+        $sql = "DELETE FROM nagrade
+                WHERE id = :id";
+
+        $stmt = $this->connection->prepare($sql);
+
+        return $stmt->execute([
+            ':id' => $id
+        ]);
+    }
+
+    public function deleteByPolaznik(
+        int $polaznikId
+    ): bool {
+        $sql = "DELETE FROM nagrade
+                WHERE polaznik_id = :polaznik_id";
+
+        $stmt = $this->connection->prepare($sql);
+
+        return $stmt->execute([
+            ':polaznik_id' => $polaznikId
+        ]);
     }
 }
